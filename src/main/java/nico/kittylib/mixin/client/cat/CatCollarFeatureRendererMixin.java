1c60cf5d543157db801274d161d2cf4ea18cb047
Nico44YT <Nico_44-YouTube@gmx.de>
2026-08-12 00:58:44 +0200

    Cleanup

--- src/main/java/nico/kittylib/mixin/client/cat/CatCollarFeatureRendererMixin.java
+++ src/main/java/nico/kittylib/mixin/client/cat/CatCollarFeatureRendererMixin.java
@@ -2,6 +2,7 @@ package nico.kittylib.mixin.client.cat;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.CatCollarFeatureRenderer;
 import net.minecraft.client.util.math.MatrixStack;
@@ -14,26 +15,27 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+
 @Mixin(CatCollarFeatureRenderer.class)
 public abstract class CatCollarFeatureRendererMixin {
 
-    @Unique
-    private CatEntity kittylib$catEntity;
-
-    @Inject(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/passive/CatEntity;FFFFFF)V", at = @At("HEAD"))
-    private void kittylib$obtainEntity(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CatEntity catEntity, float f, float g, float h, float j, float k, float l, CallbackInfo ci) {
-        this.kittylib$catEntity = catEntity;
-    }
+    private static final List<String> NAMES = List.of(
+            "nico",
+            "rainbowkitty",
+            "pride",
+            "rainbow"
+    );
 
     @WrapOperation(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/passive/CatEntity;FFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/DyeColor;getColorComponents()[F"))
-    private float[] kittylib$getColorComponents(DyeColor instance, Operation<float[]> original) {
-        if(kittylib$catEntity.hasCustomName() && ("nico".equalsIgnoreCase(kittylib$catEntity.getName().getString()) || "rainbowkitty".equalsIgnoreCase(kittylib$catEntity.getName().getString()))) {
+    private float[] kittylib$getColorComponents(DyeColor instance, Operation<float[]> original, @Local(argsOnly = true) CatEntity catEntity) {
+        if (catEntity.hasCustomName() && NAMES.contains(catEntity.getName().getString().toLowerCase())) {
             int m = 25;
-            int n = kittylib$catEntity.age / m + kittylib$catEntity.getId();
+            int n = catEntity.age / m + catEntity.getId();
             int o = DyeColor.values().length;
             int p = n % o;
             int q = (n + 1) % o;
-            float r = ((float)(kittylib$catEntity.age % m) + 0.1f) / (float)m;
+            float r = ((float) (catEntity.age % m) + 0.1f) / (float) m;
             float[] fs = SheepEntity.getRgbColor(DyeColor.byId(p));
             float[] gs = SheepEntity.getRgbColor(DyeColor.byId(q));
             fs[0] = fs[0] * (1.0F - r) + gs[0] * r;
